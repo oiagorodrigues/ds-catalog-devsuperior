e59c69844e06ba9edb4ac697957e7a6a6b18cbeb
Iago Rodrigues <yagomello16@gmail.com>
2021-05-14 00:52:47 -0300

    backend project created

---
+++ backend/src/main/java/dev/iagorodrigues/dscatalog/DscatalogApplication.java
@@ -0,0 +1,13 @@
+package dev.iagorodrigues.dscatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DscatalogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DscatalogApplication.class, args);
+    }
+
+}
