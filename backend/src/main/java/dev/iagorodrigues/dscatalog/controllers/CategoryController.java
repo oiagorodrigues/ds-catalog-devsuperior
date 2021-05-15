64ae86373295132983f98b519fc2d7f0ba61a8ef
Iago Rodrigues <yagomello16@gmail.com>
2021-05-15 15:57:35 -0300

    refactor(CategoryController): use CategoryService

--- backend/src/main/java/dev/iagorodrigues/dscatalog/controllers/CategoryController.java
+++ backend/src/main/java/dev/iagorodrigues/dscatalog/controllers/CategoryController.java
@@ -1,11 +1,13 @@
 package dev.iagorodrigues.dscatalog.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import dev.iagorodrigues.dscatalog.entities.Category;
+import dev.iagorodrigues.dscatalog.services.CategoryService;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,11 +16,11 @@ import java.util.List;
 @RequestMapping("/api/categories")
 public class CategoryController {
 
+    @Autowired
+    private CategoryService categoryService;
+
     @GetMapping
     public ResponseEntity<List<Category>> getCategories() {
-        List<Category> categories = new ArrayList<>();
-        categories.add(new Category(1L, "Books"));
-        categories.add(new Category(2L, "Electronics"));
-        return ResponseEntity.ok().body(categories);
+        return ResponseEntity.ok().body(categoryService.findAll());
     }
 }
