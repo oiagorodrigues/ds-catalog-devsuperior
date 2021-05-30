93caced6bb858e3ed37157b71e8a1c389643e834
Iago Rodrigues <yagomello16@gmail.com>
2021-05-29 23:00:32 -0300

    feat(Category) auditing data

--- backend/src/main/java/dev/iagorodrigues/dscatalog/entities/Category.java
+++ backend/src/main/java/dev/iagorodrigues/dscatalog/entities/Category.java
@@ -1,23 +1,26 @@
 package dev.iagorodrigues.dscatalog.entities;
 
 import dev.iagorodrigues.dscatalog.dto.CategoryDTO;
-import lombok.AllArgsConstructor;
+import lombok.AccessLevel;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
 import javax.persistence.Table;
+import java.time.Instant;
 
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@NoArgsConstructor
 @Getter
 @Setter
-@EqualsAndHashCode(onlyExplicitlyIncluded = true)
-@AllArgsConstructor
-@NoArgsConstructor
 @Entity
 @Table(name = "tb_category")
 public class Category {
@@ -29,8 +32,25 @@ public class Category {
 
     private String name;
 
+    @Setter(AccessLevel.NONE)
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant createdAt;
+
+    @Setter(AccessLevel.NONE)
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant updatedAt;
+
     public Category(CategoryDTO categoryDTO) {
         name = categoryDTO.getName();
     }
 
+    @PrePersist
+    public void prePersist() {
+        createdAt = Instant.now();
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        updatedAt = Instant.now();
+    }
 }
