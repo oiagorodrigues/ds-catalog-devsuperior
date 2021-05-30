0c16daaaa79fabd81ebba67fad7368a7206f829d
Iago Rodrigues <yagomello16@gmail.com>
2021-05-29 23:40:18 -0300

    feat: map Product entity

--- backend/src/main/java/dev/iagorodrigues/dscatalog/entities/Product.java
+++ backend/src/main/java/dev/iagorodrigues/dscatalog/entities/Product.java
@@ -1,24 +1,54 @@
 package dev.iagorodrigues.dscatalog.entities;
 
-import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.util.List;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import java.time.Instant;
+import java.util.HashSet;
+import java.util.Set;
 
-@AllArgsConstructor
+@Entity
+@Table(name = "tb_product")
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @NoArgsConstructor
-@Getter
-@Setter
+@Getter @Setter
 public class Product {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
     private Long id;
+
     private String name;
+
+    // defines the description as a TEXT so it can persist more strings
+    @Column(columnDefinition = "TEXT")
     private String description;
+
     private Double price;
+
     private String imgUrl;
 
-    private List<Category> categories;
+    // persist the date without time zone
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant date;
+
+    @ManyToMany
+    @JoinTable(name = "tb_product_category",
+        joinColumns = @JoinColumn(name = "product_id"),
+        inverseJoinColumns = @JoinColumn(name = "category_id"))
+    private final Set<Category> categories = new HashSet<>();
+
 
 }
