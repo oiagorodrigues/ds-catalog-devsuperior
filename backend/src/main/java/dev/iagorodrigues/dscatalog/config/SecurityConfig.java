f4aa32c27f717bd8291773e5e2bbad552ddbdee6
Iago Rodrigues <yagomello16@gmail.com>
2021-06-17 12:25:45 -0300

    chore: ignoring temporarily security

---
+++ backend/src/main/java/dev/iagorodrigues/dscatalog/config/SecurityConfig.java
@@ -0,0 +1,17 @@
+package dev.iagorodrigues.dscatalog.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+//    Configuração provisória para liberar todos endpoints
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/**");
+    }
+}
